242a4298205f702ca87f621de24d66c030dd7b10
Joseph <tannv.k55v@gmail.com>
2022-09-05 07:23:33 +0700

    Cấu Hình Routers/Layout cho Project

--- src/Alert.js
+++
@@ -1,5 +0,0 @@
-const Alert = () => {
-    alert('Say Hi!');
-};
-
-export default Alert;
--- src/components/Button/index.js
+++
@@ -1,6 +0,0 @@
-import Alert from '~/Alert';
-
-function Button() {
-    return <button onClick={Alert}>Click Me</button>;
-}
-export default Button;
